e6a0203ae73324dd077077a19a828e2d92f729d1
paula rivarola <paularivarola02@gmail.com>
2021-10-05 20:13:47 -0300

    contact

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,31 +1,33 @@
-import React from "react";
-import styles from "../styles/header.module.css";
-import { NavLink, Link } from "react-router-dom";
+import React from "react"
+import styles from "../styles/header.module.css"
+import { NavLink, Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <header>
       <nav class={styles.containerNavegation}>
-        <img
-          class={styles.logo}
-          src="../assets/logo-cocina-prueba.png"
-          alt="logo"
-        />
+          <img
+            class={styles.logo}
+            src="/assets/logo-cocina-prueba.png"
+            alt="logo"
+          />
         <div class={styles.navegation}>
-          <NavLink class={styles.active} to="/">Home</NavLink>
-          <NavLink to="/">Platos</NavLink>
+          <NavLink exact activeClassName={styles.active} to="/">
+            Home
+          </NavLink>
+          <NavLink activeClassName={styles.active} to="/product">
+            Platos
+          </NavLink>
           <NavLink to="/">Promos</NavLink>
           <NavLink to="/">Pedidos</NavLink>
-          <NavLink to="/">Contacto</NavLink>
+          <NavLink activeClassName={styles.active} to="/contact">
+            Contacto
+          </NavLink>
         </div>
-        <img
-          class={styles.user}
-          src="../assets/user.png"
-          alt="logo"
-        />
+        <img class={styles.user} src="../assets/user.png" alt="logo" />
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
