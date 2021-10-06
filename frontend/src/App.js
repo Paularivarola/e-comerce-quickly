55b48a30a021621aa456a3b8332e3c501d9ed689
Rafael Mian <rafaelmian35@gmail.com>
2021-10-06 15:15:22 -0300

    X

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 
 const App = () => {
+  // console.log(window.location)
   return (
     <BrowserRouter>
       <Header />
