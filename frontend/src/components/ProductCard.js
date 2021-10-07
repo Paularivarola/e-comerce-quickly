1992c3e55278af5a24343b94cc52fa082903d3cb
Rafael Mian <rafaelmian35@gmail.com>
2021-10-07 13:23:13 -0300

    Products

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,94 +1,65 @@
 import styles from '../styles/productCard.module.css'
-import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs"
-import { MdShoppingCart } from "react-icons/md"
+import { BsStar, BsStarHalf, BsStarFill } from 'react-icons/bs'
+import { MdShoppingCart } from 'react-icons/md'
 
-const ProductCard = () => {
+const ProductCard = ({ product, setModal }) => {
+  let star = (
+    <BsStar
+      style={{
+        color: 'rgb(216, 205, 56)',
+        fontSize: '1.3em',
+        marginRight: '5%',
+      }}
+    />
+  )
+  let starHalf = (
+    <BsStarHalf
+      style={{
+        color: 'rgb(216, 205, 56)',
+        fontSize: '1.3em',
+        marginRight: '5%',
+      }}
+    />
+  )
+  let starFill = (
+    <BsStarFill
+      style={{
+        color: 'rgb(216, 205, 56)',
+        fontSize: '1.3em',
+        marginRight: '5%',
+      }}
+    />
+  )
 
-    let star = <BsStar style={{ color: 'rgb(216, 205, 56)', fontSize: '1.3em', marginRight: '5%' }}/>
-    let starHalf = <BsStarHalf style={{ color: 'rgb(216, 205, 56)', fontSize: '1.3em', marginRight: '5%' }}/>
-    let starFill = <BsStarFill style={{ color: 'rgb(216, 205, 56)', fontSize: '1.3em', marginRight: '5%' }}/>
-
-    let products = [
-        {
-            img: '/assets/pizzas.jpeg',
-            name: 'nombre producto',
-            category: 'categoria',
-            description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-            price: '550',
-            ingredients: 'jamon, tomate, muzzarella',
-            stock: 'ni idea',
-        },
-        {
-            img: '/assets/pizzas.jpeg',
-            name: 'nombre producto',
-            category: 'categoria',
-            description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-            price: '550',
-            ingredients: 'jamon, tomate, muzzarella',
-            stock: 'ni idea',
-        },
-        {
-            img: '/assets/pizzas.jpeg',
-            name: 'nombre producto',
-            category: 'categoria',
-            description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-            price: '550',
-            ingredients: 'jamon, tomate, muzzarella',
-            stock: 'ni idea',
-        },
-        {
-            img: '/assets/pizzas.jpeg',
-            name: 'nombre producto',
-            category: 'categoria',
-            description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-            price: '550',
-            ingredients: 'jamon, tomate, muzzarella',
-            stock: 'ni idea',
-        },
-        {
-            img: '/assets/pizzas.jpeg',
-            name: 'nombre producto',
-            category: 'categoria',
-            description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-            price: '550',
-            ingredients: 'jamon, tomate, muzzarella',
-            stock: 'ni idea',
-        },
-    ]
-        
-    products = [...products, ...products, ...products, ...products, ...products, ...products]
-    return (
-        <>
-            <div className={styles.products}>
-                {products.map((product, index) => 
-
-                <div className={styles.product} key={index}>
-                    <div className={styles.productBox}>
-                        <div className={styles.productImg} style={{ backgroundImage: `url("${product.img}")` }}></div>
-                        <div className={styles.productInfo}>
-                            <h3 className={styles.productName}>{product.name}</h3>
-                            <div></div>
-                            <p className={styles.productDesc}>{product.description}</p>
-                            <p className={styles.productIng}>{product.ingredients}</p>
-                        </div>
-                    </div>
-                    <hr className={styles.line}></hr>
-                    <div className={styles.priceBox}>
-                        <p className={styles.price}><span className={styles.priceTitle}>Price: </span>$ {product.price}</p>
-                        <div className={styles.calification}>
-                            {starFill}
-                            {starFill}
-                            {starFill}
-                            {starHalf}
-                            {star}
-                        </div>
-                    </div>
-                    <button className={styles.addBtn}><MdShoppingCart style={{ color: 'white', fontSize: '1.8em', marginRight: '5%' }}/> +</button>
-                </div>
-            )}
+  return (
+    <div className={styles.product}>
+      <div className={styles.productBox}>
+        <div className={styles.productImg} style={{ backgroundImage: `url("${product.img}")` }}></div>
+        <div className={styles.productInfo}>
+          <h3 className={styles.productName}>{product.name}</h3>
+          <div></div>
+          <p className={styles.productDesc}>{product.description}</p>
+          <p className={styles.productIng}>{product.ingredients}</p>
         </div>
-        </>
-    )
+      </div>
+      <hr className={styles.line}></hr>
+      <div className={styles.priceBox}>
+        <p className={styles.price}>
+          <span className={styles.priceTitle}>Price: </span>$ {product.price}
+        </p>
+        <div className={styles.calification}>
+          {starFill}
+          {starFill}
+          {starFill}
+          {starHalf}
+          {star}
+        </div>
+      </div>
+      <button className={styles.addBtn}>
+        <MdShoppingCart style={{ color: 'white', fontSize: '1.8em', marginRight: '5%' }} onClick={() => setModal(true, product)} /> +
+      </button>
+    </div>
+  )
 }
 
 export default ProductCard
