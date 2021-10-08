989af5db93d044a2be87e3b0801a26bac83c7585
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-10-08 09:29:29 -0300

    Merge Nicole

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -5,97 +5,40 @@ import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import { connect } from "react-redux";
 
-const ProductCard = (props) => {
-
-  let products = [
-    {
-      img: '/assets/pizzas.jpeg',
-      name: 'nombre producto',
-      category: 'categoria',
-      description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-      price: '550',
-      ingredients: 'jamon, tomate, muzzarella',
-      stock: 'ni idea',
-    },
-    {
-      img: '/assets/pizzas.jpeg',
-      name: 'nombre producto',
-      category: 'categoria',
-      description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-      price: '550',
-      ingredients: 'jamon, tomate, muzzarella',
-      stock: 'ni idea',
-    },
-    {
-      img: '/assets/pizzas.jpeg',
-      name: 'nombre producto',
-      category: 'categoria',
-      description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-      price: '550',
-      ingredients: 'jamon, tomate, muzzarella',
-      stock: 'ni idea',
-    },
-    {
-      img: '/assets/pizzas.jpeg',
-      name: 'nombre producto',
-      category: 'categoria',
-      description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-      price: '550',
-      ingredients: 'jamon, tomate, muzzarella',
-      stock: 'ni idea',
-    },
-    {
-      img: '/assets/pizzas.jpeg',
-      name: 'nombre producto',
-      category: 'categoria',
-      description: 'Acá va a ir toda la descripción del producto que quieran comprar la gentessss',
-      price: '550',
-      ingredients: 'jamon, tomate, muzzarella',
-      stock: 'ni idea',
-    },
-  ]
-
-  products = [...products, ...products, ...products, ...products, ...products, ...products]
+const ProductCard = ({ product, setModal }) => {
   return (
-    <>
-      <div className={styles.products}>
-        {products.map((product, index) =>
-
-          <div className={styles.product} key={index}>
-            <div className={styles.productBox}>
-              <div className={styles.productImg} style={{ backgroundImage: `url("${product.img}")` }}></div>
-              <div className={styles.productInfo}>
-                <h3 className={styles.productName}>{product.name}</h3>
-                <div></div>
-                <p className={styles.productDesc}>{product.description}</p>
-                <p className={styles.productIng}>{product.ingredients}</p>
-              </div>
-            </div>
-            <hr className={styles.line}></hr>
-            <div className={styles.priceBox}>
-              <p className={styles.price}><span className={styles.priceTitle}>Price: </span>$ {product.price}</p>
-              <div className={styles.calification}>
-                <Stack spacing={1}>
-                  {!props.user ? <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly /> : <Rating className={styles.rating} style={{ backgroundColor: 'yelow' }} name="half-rating" defaultValue={2.5} precision={0.5} />}
-                </Stack>
-              </div>
-            </div>
-            <button className={styles.addBtn}><MdShoppingCart style={{ color: 'white', fontSize: '1.8em', marginRight: '5%' }} /> +</button>
-          </div>
-        )}
+    <div className={styles.product}>
+      <div className={styles.productBox}>
+        <div className={styles.productImg} style={{ backgroundImage: url("${product.img}") }}></div>
+        <div className={styles.productInfo}>
+          <h3 className={styles.productName}>{product.name}</h3>
+          <div></div>
+          <p className={styles.productDesc}>{product.description}</p>
+          <p className={styles.productIng}>{product.ingredients}</p>
+        </div>
       </div>
-      <div >
-        <button data-modal='productModal' className={styles.addBtn} onClick={() => setModal(true, product)}>
-          <MdShoppingCart style={{ color: 'white', fontSize: '1.8em', marginRight: '5%' }} /> +
-        </button>
-      </div >
-    </>
+      <hr className={styles.line}></hr>
+      <div className={styles.priceBox}>
+        <p className={styles.price}>
+          <span className={styles.priceTitle}>Price: </span>$ {product.price}
+        </p>
+        <div className={styles.calification}>
+          <Stack spacing={1}>
+            {!props.user ? <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly /> : <Rating className={styles.rating} style={{ backgroundColor: 'yelow' }} name="half-rating" defaultValue={2.5} precision={0.5} />}
+          </Stack>
+        </div>
+
+      </div>
+      <button data-modal='productModal' className={styles.addBtn} onClick={() => setModal(true, product)}>
+        <MdShoppingCart style={{ color: 'white', fontSize: '1.8em', marginRight: '5%' }} /> +
+      </button>
+    </div>
   )
 }
 
 const mapStateToProps = state => {
   return {
-    user: state.users.user,
+    user: state.users.user
   }
 }
 
