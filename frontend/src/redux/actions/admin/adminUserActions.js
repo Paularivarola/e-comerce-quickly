f0edc6c456107a2f951c3803e1697e32bcbb5a81
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-10-08 09:03:53 -0300

    Admin Panel

---
+++ frontend/src/redux/actions/admin/adminUserActions.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+
+const adminUsersActions = {
+    getUsers: () => {
+        let token = localStorage.getItem("token");
+        return async (dispatch) => {
+            let response = await axios.get("http://localhost:4000/api/admin/users", {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            });
+            if (response.data.success) {
+                dispatch({ type: "GET_USERS", payload: response.data.response });
+                return response.data;
+            }
+        };
+    },
+    createUser: (user) => {
+        let token = localStorage.getItem("token");
+        return async (dispatch) => {
+            let response = await axios.post(
+                "http://localhost:4000/api/admin/users",
+                user,
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+            if (response.data.success) {
+                await dispatch({ type: "ADD_USER", payload: response.data.response });
+                return response.data;
+            }
+        };
+    },
+    updateUser: (newUser, userId) => {
+        let token = localStorage.getItem("token");
+        return async (dispatch) => {
+            let response = await axios.put(
+                "http://localhost:4000/api/admin/user/" + userId,
+                newUser,
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+            if (response.data.success) {
+                await dispatch({ type: "UPDATE_USER", payload: newUser });
+                return response.data;
+            }
+        };
+    },
+
+    deleteUser: (userId) => {
+        let token = localStorage.getItem("token");
+        return async (dispatch) => {
+            let response = await axios.delete(
+                "http://localhost:4000/api/admin/user/" + userId,
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+            console.log(response)
+            if (response.data.success) {
+                await dispatch({ type: "DELETE_USER", payload: userId });
+                return response.data;
+            }
+        };
+    },
+    // getReviews: () => {
+    //     return async (dispatch) => {
+    //         let response = await axios.get(
+    //             "http://localhost:4000/api/review"
+    //         );
+    //         if (response.data.success) {
+    //             dispatch({ type: "GET_REVIEWS", payload: response.data.response });
+    //             return response.data;
+    //         }
+    //     };
+    // },
+};
+export default adminUsersActions;
