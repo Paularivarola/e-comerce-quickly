10e22c8396129e24e5a7cb8bdc6571c2a6e2485a
Mica <mica@mica.com>
2021-10-09 18:18:12 -0300

    arreglos

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,21 +7,29 @@ import Rating from '@mui/material/Rating'
 import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import { urlencoded } from 'express'
 
 const Product = ({ product, setMod, user, manageCart, ...props }) => {
   const sizeFries = [
     { size: 'Chicas', cost: 0 },
     { size: 'Medianas', cost: 10 },
     { size: 'Grandes', cost: 20 },
-  ] //debería venir de props y si no existe debe ser []
+  ]
   const extrasChoices = [
     { type: 'Carne', cost: 20 },
     { type: 'Queso', cost: 10 },
     { type: 'Cebolla', cost: 5 },
     { type: 'Gaseosa 500cc', cost: 35 },
-  ] //debería venir de props y estar siempre (aunque sea una gaseosa)
+  ]
+  const drinksChoices = [
+    {type: "Sin bebida", cost: 0},
+    {type: "Coca Cola (500cc)" , cost: 100},
+    {type: "Sprite (500cc)", cost: 100},
+    {type: "Fanta (500cc)", cost: 100}
+  ]
 
   const [fries, setFries] = useState('Chicas')
+  const [drink, setDrink] = useState('Sin bebida')
   const [aclaraciones, setAclaraciones] = useState('')
   const [extras, setExtras] = useState([])
   const [extrasCost, setExtrasCost] = useState(0)
@@ -45,6 +53,10 @@ const Product = ({ product, setMod, user, manageCart, ...props }) => {
     setFries(fries)
   }
 
+  const addDrink = (drink) => {
+    setDrink(drink)
+  }
+
   const addExtras = (extra) => {
     if (!extras.includes(extra)) {
       setExtras([...extras, extra])
@@ -63,8 +75,9 @@ const Product = ({ product, setMod, user, manageCart, ...props }) => {
 
   useEffect(() => {
     let friesCost = sizeFries.find((size) => size.size === fries).cost
-    setUnitaryPrice(product.price + friesCost + extrasCost)
-  }, [sizeFries, extrasCost])
+    let drinkCost = drinksChoices.find((option) => option.type === drink).cost
+    setUnitaryPrice(product.price + friesCost + extrasCost + drinkCost)
+  }, [sizeFries, extrasCost, drink])
 
   useEffect(() => {
     setTotalPrice(unitaryPrice * totalAmount)
@@ -86,62 +99,61 @@ const Product = ({ product, setMod, user, manageCart, ...props }) => {
     <main data-modal='closeModal' className={styles.main}>
       <div className={styles.card}>
         <ImCancelCircle className={styles.exit} onClick={() => setMod(false)} />
-
-        <div className={styles.product}>
-          <div className={styles.cardInfo}>
-            <div className={styles.title}>
-              <h1>{product.name}</h1>
-              <Stack className={styles.calification} spacing={1}>
-                {user ? (
-                  <Rating
-                    className={styles.rating}
-                    style={{ backgroundColor: 'yelow' }}
-                    name='half-rating'
-                    defaultValue={product.score}
-                    precision={0.5}
-                  />
-                ) : (
-                  <Rating
-                    name='half-rating-read'
-                    defaultValue={product.score}
-                    precision={0.5}
-                    readOnly
-                  />
-                )}
-              </Stack>
-            </div>
-
-            <div className={styles.title}>
-              <h3>Descripcion:</h3>
-              <p>{product.description}</p>
-            </div>
-
-            <div className={styles.order}>
-              <div className={styles.amount}>
-                <p
-                  className={styles.amountButton}
-                  onClick={() => amount('res')}
-                >
-                  -
-                </p>
-                <p>{totalAmount}</p>
-                <p
-                  className={styles.amountButton}
-                  onClick={() => amount('sum')}
-                >
-                  +
-                </p>
-              </div>
-              <p className={styles.addToCart} onClick={addToCart}>
-                Agregar a mi orden
-              </p>
-            </div>
+        <div className={styles.cardInfo}>
+          <div>
+            <h1 className={styles.h3}>{product.name}</h1>
+            <Stack className={styles.calification} spacing={1}>
+              {user ? (
+                <Rating
+                  className={styles.rating}
+                  style={{ backgroundColor: 'yelow' }}
+                  name='half-rating'
+                  defaultValue={product.score}
+                  precision={0.5}
+                />
+              ) : (
+                <Rating
+                  name='half-rating-read'
+                  defaultValue={product.score}
+                  precision={0.5}
+                  readOnly
+                />
+              )}
+            </Stack>
           </div>
 
-          <img className={styles.cardPicture} src={product.img} />
+          <div className={styles.div}>
+            <div style={{backgroundImage:`url('${product.img}')`}} className={styles.picture}/>
+            <h3 className={styles.h3}>Descripcion:</h3>
+            <p className={styles.text}>{product.description}</p>
+          </div>
 
-          <div className={styles.cardPrice}>
-            <div className={styles.choices}>
+          <div className={styles.order}>
+            <div className={styles.amount}>
+              <p
+                className={styles.amountButton}
+                onClick={() => amount('res')}
+              >
+                -
+              </p>
+              <p>{totalAmount}</p>
+              <p
+                className={styles.amountButton}
+                onClick={() => amount('sum')}
+              >
+                +
+              </p>
+            </div>
+            <p className={styles.addToCart} onClick={addToCart}>
+              Agregar a mi orden
+            </p>
+          </div>
+        </div>
+
+
+        <div className={styles.cardPrice}>
+          <div className={styles.choices}>
+            {product.papas && product.extras && <div className={styles.column}>
               {product.papas && (
                 <div>
                   <h3 className={styles.h3}>Tamaño papas</h3>
@@ -161,7 +173,7 @@ const Product = ({ product, setMod, user, manageCart, ...props }) => {
                         {size.cost !== 0 && (
                           <span className={styles.span}>
                             {' '}
-                            (USD {size.cost})
+                            ${size.cost}
                           </span>
                         )}
                       </label>
@@ -169,32 +181,57 @@ const Product = ({ product, setMod, user, manageCart, ...props }) => {
                   ))}
                 </div>
               )}
+            {product.extras && (
               <div>
-                {product.extras && (
-                  <>
-                    <h3 className={styles.h3}>Extras</h3>
-                    {extrasChoices.map((extra, index) => (
-                      <div key={index}>
-                        <input
-                          type='checkbox'
-                          name='extras'
-                          value={extra.type}
-                          id={extra.type}
-                          onClick={() => addExtras(extra.type)}
-                        />
+                <h3 className={styles.h3}>Extras</h3>
+                {extrasChoices.map((extra, index) => (
+                  <div key={index}>
+                    <input
+                      type='checkbox'
+                      name='extras'
+                      value={extra.type}
+                      id={extra.type}
+                      onClick={() => addExtras(extra.type)}
+                    />
 
-                        <label className={styles.input} htmlFor={extra.type}>
-                          {extra.type}{' '}
-                          <span className={styles.span}>
-                            (USD {extra.cost})
-                          </span>
-                        </label>
-                      </div>
-                    ))}
-                  </>
-                )}
+                    <label className={styles.input} htmlFor={extra.type}>
+                      {extra.type}{' '}
+                      <span className={styles.span}>
+                        ${extra.cost}
+                      </span>
+                    </label>
+                  </div>
+                ))}
               </div>
-              <Box
+            )}
+            </div>}
+            <div className={(product.papas && product.extras) ? styles.column : styles.no_column}>
+            <div>
+              <h3 className={styles.h3}>Gaseosa</h3>
+              {drinksChoices.map((option, index) => (
+                <div key={index}>
+                  <input
+                    type='radio'
+                    name='extras'
+                    value={option.type}
+                    id={option.type}
+                    onClick={() => addDrink(option.type)}
+                    defaultChecked={option.cost === 0 && 'checked'}
+                  />
+
+                  <label className={styles.input} htmlFor={option.type}>
+                    {option.type}
+                    {option.cost !== 0 && (
+                      <span className={styles.span}>
+                        {' '}
+                        ${option.cost}
+                      </span>
+                    )}
+                  </label>
+                </div>
+              ))}
+            </div>
+            <Box
                 component='form'
                 sx={{
                   '& .MuiTextField-root': {
@@ -205,7 +242,7 @@ const Product = ({ product, setMod, user, manageCart, ...props }) => {
                 }}
                 noValidate
                 autoComplete='off'
-              >
+            >
                 <TextField
                   id='outlined-multiline-flexible'
                   label='Aclaraciones'
@@ -215,12 +252,12 @@ const Product = ({ product, setMod, user, manageCart, ...props }) => {
                   value={aclaraciones}
                   onChange={(e) => setAclaraciones(e.target.value)}
                 />
-              </Box>
+            </Box>
             </div>
-            <div>
-              <h4>Unidad: $ {unitaryPrice}</h4>
-              <h2>Total: $ {totalPrice}</h2>
-            </div>
+          </div>
+          <div>
+            <h4>Unidad: $ {unitaryPrice}</h4>
+            <h2>Total: $ {totalPrice}</h2>
           </div>
         </div>
       </div>
