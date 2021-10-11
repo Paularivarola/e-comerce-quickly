7aeaec8bfc37bd7bd155b6b4a5e0a0c80563e02d
Cristian Tortoza <tortozacris@gmail.com>
2021-10-11 12:37:57 -0300

    tostaditas

--- frontend/src/components/CardTost.js
+++ frontend/src/components/CardTost.js
@@ -2,26 +2,32 @@ import { useEffect, useState } from "react"
 import styles from '../styles/cardTost.module.css'
 
 
-const CardTost = () => {
-	const [typeTost, setTypeTost] = useState("good")
+const CardTost = ({properties, setCardTost}) => {
+	const {time, icon, text, view} = properties
+	const [typeTost, setTypeTost] = useState(icon)
 	const [imageTost, setImageTost] = useState("")
 	const [iconTost, setIconTost] = useState("")
-	const [textTost, setTextTost] = useState("vuelve pronto")
-	const [render, setRender] = useState(false)
+	const [textTost, setTextTost] = useState(text)
+	const [render, setRender] = useState(view)
 
 	useEffect(() => {
 		// setRender(pasar por props, una variable de estado en false, y que pase a true cuando este pase a true.)
 	},[])
 
-	if(render){
+	if(render){	
 		setTimeout(() => {
 			setRender(false)
-		}, 1500)
+			setCardTost({
+				icon: "",
+				text: "",
+				view: false,
+			})
+		}, time)
 	}
 	
 
 	useEffect(() => {
-		if(typeTost === "good"){
+		if(typeTost === "success"){
 			setImageTost("https://i.postimg.cc/4dr1ZcmV/papasfritas1.jpg")
 			setIconTost("https://i.postimg.cc/J4nh2sQx/ok.png")
 		}else if(typeTost === "error"){
@@ -40,13 +46,11 @@ const CardTost = () => {
 	return(
 		<div className={styles.containCard}>
 			<div className={styles.imageIcon} style={{backgroundImage: `url("${iconTost}")`}}> 
-
 			</div>
 			<div>
 				<h4>{textTost}</h4>
 			</div>
 			<div className={styles.imageTost} style={{backgroundImage: `url("${imageTost}")`}}>
-
 			</div>
 		</div>
 	)
